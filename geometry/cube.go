4614fc93b4a9de387eb0e70eceb0b98bc21375d2
psaumur <psaumur@gmail.com>
2022-05-27 18:00:42 -0700

    Forgot to add 'nil;

--- geometry/cube.go
+++ geometry/cube.go
@@ -4,7 +4,7 @@ import "errors"
 
 func CubeVolume(n int) (int, error) {
 	if n != 0 {
-		return n * n * n
+		return n * n * n, nil
 	} else {
 		return 0, errors.New("Zero length edge is not allowed")
 	}
