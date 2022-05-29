8c8dd99834a46905ffb61589e201ab6fba2bc2a2
psaumur <psaumur@gmail.com>
2022-05-28 19:46:53 -0700

    Does this work?

--- arithmetic/arithmetic.go
+++ arithmetic/arithmetic.go
@@ -9,3 +9,14 @@ func IsPrime(num int) bool {
 	}
 	return true
 }
+func Factorial(n int) int {
+	var f int = 1
+	for i := 2; i <= n; i++ {
+		f *= i
+	}
+	return f
+}
+
+func Add(n int) int {
+	return n + 1000
+}
